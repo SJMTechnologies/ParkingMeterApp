aa8d9325ef955957f7b00c8d28abaf1358cc9128
pom.incorporated@gmail.com <y76rWq83>
2017-01-17 19:33:08 +0530

    Sending Payment Parameter

---
+++ app/src/main/java/com/park/parkingmeterapp/activities/PaymentActivity.java
@@ -0,0 +1,59 @@
+package com.park.parkingmeterapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebView;
+
+import com.park.parkingmeterapp.ParkApp;
+import com.park.parkingmeterapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @BindView(R.id.webView)
+    WebView webView;
+    String strTime = "";
+    String strAmount = "";
+    String strArea = "";
+    String strPost = "";
+    String strlatitude = "";
+    String strLongitude = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        ButterKnife.bind(this);
+
+        if (getIntent().getExtras() != null) {
+            strTime = getIntent().getExtras().getString(getString(R.string.time));
+            strAmount = getIntent().getExtras().getString(getString(R.string.amount));
+            strArea = getIntent().getExtras().getString(getString(R.string.area));
+            strPost = getIntent().getExtras().getString(getString(R.string.post));
+            strlatitude = getIntent().getExtras().getString(getString(R.string.latitude));
+            strLongitude = getIntent().getExtras().getString(getString(R.string.longitude));
+        }
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        Log.e("Url", "http://54.162.93.65/services/ekashu/index.php?auth_token=" + ParkApp.preferences.getAuthToken() + "&fname=" + ParkApp.preferences.getUSER_FNAME() + "&lname=" + ParkApp.preferences.getUSER_lNAME() + "&address=" + ParkApp.preferences.getUSER_ADDRESS1() + "&city=" + ParkApp.preferences.getUSER_CITY() + "&zone_id=" + ParkApp.preferences.getUSER_STATE() + "&zip=" + ParkApp.preferences.getUSER_ZIP() + "&country_id=" + ParkApp.preferences.getUSER_COUNTRY() + "&email=" + ParkApp.preferences.getUSER_EMAIL()
+               + "&longitude=" + strLongitude + "&latitude=" + strlatitude + "&area=" + strArea + "&post=" + strPost + "&total_time=" + strTime + "" + "&amount=" + strAmount + "");
+ //       Log.e("url2", "http://54.162.93.65/services/ekashu/index.php?auth_token=" + ParkApp.preferences.getAuthToken() + "&fname=" + "Ramesh" + "&lname=" + "Solanki" + "&address=" + "D302 Parswanath" + "&city=" + "Ahmedabad" + "&zone_id=" + "Gujarat" + "&zip=" + "382424" + "&country_id=" + "India" + "&email=" + ParkApp.preferences.getUSER_EMAIL()
+   //             + "&longitude=" + strLongitude + "&latitude=" + strlatitude + "&area=" + strArea + "&post=" + strPost + "&total_time=" + strTime + "" + "&amount=" + strAmount + "");
+//        webView.loadUrl("http://54.162.93.65/services/ekashu/index.php?auth_token=" + ParkApp.preferences.getAuthToken() + "&fname=" + ParkApp.preferences.getUSER_FNAME() + "&lname=" + ParkApp.preferences.getUSER_lNAME() + "&address=" + ParkApp.preferences.getUSER_ADDRESS() + "&city=" + ParkApp.preferences.getUSER_CITY() + "&zone_id=" + ParkApp.preferences.getUSER_STATE() + "&zip=" + ParkApp.preferences.getUSER_ZIP() + "&country_id=" + ParkApp.preferences.getUSER_COUNTRY() + "&email=" + ParkApp.preferences.getUSER_EMAIL()
+//                + "&longitude=" + strLongitude + "&latitude=" + strlatitude + "&area=" + strArea + "&post=" + strPost + "&total_time=" + strTime + "" + "&amount=" + strAmount + "");
+        webView.loadUrl("http://54.162.93.65/services/ekashu/index.php?auth_token=" + ParkApp.preferences.getAuthToken() + "&fname=" + ParkApp.preferences.getUSER_FNAME() + "&lname=" + ParkApp.preferences.getUSER_lNAME() + "&address=" + ParkApp.preferences.getUSER_ADDRESS1() + "&city=" + ParkApp.preferences.getUSER_CITY() + "&zone_id=" + ParkApp.preferences.getUSER_STATE() + "&zip=" + ParkApp.preferences.getUSER_ZIP() + "&country_id=" + ParkApp.preferences.getUSER_COUNTRY() + "&email=" + ParkApp.preferences.getUSER_EMAIL()
+        + "&longitude=" + strLongitude + "&latitude=" + strlatitude + "&area=" + strArea + "&post=" + strPost + "&total_time=" + strTime + "" + "&amount=" + strAmount + "");
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(PaymentActivity.this, MainActivity.class));
+    }
+}
